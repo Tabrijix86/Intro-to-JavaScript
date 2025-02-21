1c7dc1c2eeffa8effaaa9cd049d811a17a13961f
Tabriji <tabrijiummi@gmail.com>
2025-02-22 01:20:35 +0600

    more code

---
+++ operations.js
@@ -0,0 +1,12 @@
+let value = 3;
+let negativeValue = -value;
+
+console.log(3 + negativeValue);
+
+let str1 = "Tabriji";
+let str2 = " Islam";
+
+console.log(str1 + str2);
+
+console.log(1 + 2 + "3");
+console.log(1 + 2 + "3" + 4 + 5); // JavaScript evaluates expressions from left to right.
---
+++ testing.js
@@ -0,0 +1,7 @@
+console.log(3 + 4 + "abc");
+let score = "10ab";
+console.log(typeof score);
+
+let scoreInNumber = Number(score);
+console.log(typeof scoreInNumber);
+console.log(scoreInNumber);
