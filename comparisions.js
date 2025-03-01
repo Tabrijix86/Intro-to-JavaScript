1c7d528c20452096159cc4821709a693c54d4c37
Tabriji <tabrijiummi@gmail.com>
2025-03-01 23:36:15 +0600

    Create comparisions.js

---
+++ comparisions.js
@@ -0,0 +1,10 @@
+console.log(2 > 1)
+console.log(1 / 0)
+console.log("2" > 1) // JavaScript converts the string "2" to a number before comparing.
+console.log("2" < "12") // When comparing strings, "2" is greater than "12" because 2 is greater than 1.
+console.log(null == 0); // null equals only null or undefined.
+console.log(null < 0); // null is converted to 0 during a numeric comparison.
+console.log(null == undefined); // null equals only null or undefined.
+console.log(null === undefined); // null equals only null or undefined.
+console.log(null >= 0); // null is converted to 0 during a numeric comparison.
+console.log(undefined == 0); // undefined equals only null or undefined.
