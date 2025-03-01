fc7fb0c54eb6dea77ab735ca0cca3c3d46d8772a
Tabriji <tabrijiummi@gmail.com>
2025-03-02 00:04:38 +0600

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+const score = 1000000000000000000000000n;
+const cgpa = 3.5;
+let email;
+
+const students = ["tabriji", "salma"]
+
+let Student = {
+    name: "Tabriji",
+    id: 2030660,
+    cgpa: 3.5
+}
+
+console.log(typeof score);
+console.log(typeof cgpa);
+console.log(typeof students);
+console.log(typeof Student);
